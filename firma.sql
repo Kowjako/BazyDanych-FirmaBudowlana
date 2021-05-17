d1a2d9eaf8c6a0d7459befa9267fa12529edbd5e
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-05-17 17:01:00 +0200

    Create logger tabel and triggers for it

--- firma.sql
+++ firma.sql
@@ -372,3 +372,230 @@ INSERT INTO expenses (expenses_id, order_id, company_id, payment_date, amount, m
 INSERT INTO expenses (expenses_id, order_id, company_id, payment_date, amount, material) VALUES (28, 18, 4, '01/03/18', 3, 'Izolacja cieplna');
 INSERT INTO expenses (expenses_id, order_id, company_id, payment_date, amount, material) VALUES (29, 39, 4, '21/03/18', 2, 'Zaprawy cementowe');
 INSERT INTO expenses (expenses_id, order_id, company_id, payment_date, amount, material) VALUES (30, 3, 3, '13/03/19', 4, 'Kleje do systemów ociepleń');
+
+/* Tworzenie triggerow dla tabeli logow */
+CREATE TABLE logger (
+	log_id NUMBER NOT NULL,
+	log_operation VARCHAR2(20) NOT NULL,
+	change_table VARCHAR2(20) NOT NULL,
+	change_date DATE NOT NULL
+);
+
+/* Sekwencja dla wstawiania id logow */
+CREATE SEQUENCE logger_id MINVALUE 0 MAXVALUE 999 START WITH 0 INCREMENT BY 1;
+
+ALTER TABLE logger ADD CONSTRAINT logger_pk PRIMARY KEY (log_id);
+ALTER TABLE logger ADD CONSTANT logger_check1 CHECK (log_operation IN ('Insert', 'Update', 'Delete'));
+
+/* Trigger dla tabeli company */
+CREATE OR REPLACE TRIGGER COMPANY_CHANGES_TRIGGER
+  AFTER INSERT OR UPDATE OR DELETE
+  ON company
+DECLARE
+  log_action  logger.log_operation%TYPE;
+BEGIN
+  IF INSERTING THEN
+    log_action := 'Insert';
+  ELSIF UPDATING THEN
+    log_action := 'Update';
+  ELSIF DELETING THEN
+    log_action := 'Delete';
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('This code is not reachable.');
+  END IF;
+
+  INSERT INTO logger (log_id, log_operation, change_table, change_date)
+    VALUES (logger_id.nextval, log_action, 'Company', SYSDATE);
+END;
+
+/* Trigger dla tabeli payment*/
+CREATE OR REPLACE TRIGGER PAYMENTS_CHANGES_TRIGGER
+  AFTER INSERT OR UPDATE OR DELETE
+  ON payments
+DECLARE
+  log_action  logger.log_operation%TYPE;
+BEGIN
+  IF INSERTING THEN
+    log_action := 'Insert';
+  ELSIF UPDATING THEN
+    log_action := 'Update';
+  ELSIF DELETING THEN
+    log_action := 'Delete';
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('This code is not reachable.');
+  END IF;
+
+  INSERT INTO logger (log_id, log_operation, change_table, change_date)
+    VALUES (logger_id.nextval, log_action, 'Payments', SYSDATE);
+END;
+
+/* Trigger dla tabeli expenses*/
+CREATE OR REPLACE TRIGGER EXPENSES_CHANGES_TRIGGER
+  AFTER INSERT OR UPDATE OR DELETE
+  ON expenses
+DECLARE
+  log_action  logger.log_operation%TYPE;
+BEGIN
+  IF INSERTING THEN
+    log_action := 'Insert';
+  ELSIF UPDATING THEN
+    log_action := 'Update';
+  ELSIF DELETING THEN
+    log_action := 'Delete';
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('This code is not reachable.');
+  END IF;
+
+  INSERT INTO logger (log_id, log_operation, change_table, change_date)
+    VALUES (logger_id.nextval, log_action, 'Expenses', SYSDATE);
+END;
+
+/* Trigger dla tabeli profit */
+CREATE OR REPLACE TRIGGER PROFIT_CHANGES_TRIGGER
+  AFTER INSERT OR UPDATE OR DELETE
+  ON profit
+DECLARE
+  log_action  logger.log_operation%TYPE;
+BEGIN
+  IF INSERTING THEN
+    log_action := 'Insert';
+  ELSIF UPDATING THEN
+    log_action := 'Update';
+  ELSIF DELETING THEN
+    log_action := 'Delete';
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('This code is not reachable.');
+  END IF;
+
+  INSERT INTO logger (log_id, log_operation, change_table, change_date)
+    VALUES (logger_id.nextval, log_action, 'Profit', SYSDATE);
+END;
+
+/* Trigger dla tabeli orders */
+CREATE OR REPLACE TRIGGER ORDERS_CHANGES_TRIGGER
+  AFTER INSERT OR UPDATE OR DELETE
+  ON orders
+DECLARE
+  log_action  logger.log_operation%TYPE;
+BEGIN
+  IF INSERTING THEN
+    log_action := 'Insert';
+  ELSIF UPDATING THEN
+    log_action := 'Update';
+  ELSIF DELETING THEN
+    log_action := 'Delete';
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('This code is not reachable.');
+  END IF;
+
+  INSERT INTO logger (log_id, log_operation, change_table, change_date)
+    VALUES (logger_id.nextval, log_action, 'Orders', SYSDATE);
+END;
+
+/* Trigger dla tabeli clients */
+CREATE OR REPLACE TRIGGER CLIENTS_CHANGES_TRIGGER
+  AFTER INSERT OR UPDATE OR DELETE
+  ON clients
+DECLARE
+  log_action  logger.log_operation%TYPE;
+BEGIN
+  IF INSERTING THEN
+    log_action := 'Insert';
+  ELSIF UPDATING THEN
+    log_action := 'Update';
+  ELSIF DELETING THEN
+    log_action := 'Delete';
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('This code is not reachable.');
+  END IF;
+
+  INSERT INTO logger (log_id, log_operation, change_table, change_date)
+    VALUES (logger_id.nextval, log_action, 'Clients', SYSDATE);
+END;
+
+/* Trigger dla tabeli workers */
+CREATE OR REPLACE TRIGGER WORKERS_CHANGES_TRIGGER
+  AFTER INSERT OR UPDATE OR DELETE
+  ON workers
+DECLARE
+  log_action  logger.log_operation%TYPE;
+BEGIN
+  IF INSERTING THEN
+    log_action := 'Insert';
+  ELSIF UPDATING THEN
+    log_action := 'Update';
+  ELSIF DELETING THEN
+    log_action := 'Delete';
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('This code is not reachable.');
+  END IF;
+
+  INSERT INTO logger (log_id, log_operation, change_table, change_date)
+    VALUES (logger_id.nextval, log_action, 'Workers', SYSDATE);
+END;
+	
+
+/* Trigger dla tabeli people */
+CREATE OR REPLACE TRIGGER PEOPLE_CHANGES_TRIGGER
+  AFTER INSERT OR UPDATE OR DELETE
+  ON people
+DECLARE
+  log_action  logger.log_operation%TYPE;
+BEGIN
+  IF INSERTING THEN
+    log_action := 'Insert';
+  ELSIF UPDATING THEN
+    log_action := 'Update';
+  ELSIF DELETING THEN
+    log_action := 'Delete';
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('This code is not reachable.');
+  END IF;
+
+  INSERT INTO logger (log_id, log_operation, change_table, change_date)
+    VALUES (logger_id.nextval, log_action, 'People', SYSDATE);
+END;
+
+/* Trigger dla tabeli operations */
+CREATE OR REPLACE TRIGGER OPERATIONS_CHANGES_TRIGGER
+  AFTER INSERT OR UPDATE OR DELETE
+  ON operations
+DECLARE
+  log_action  logger.log_operation%TYPE;
+BEGIN
+  IF INSERTING THEN
+    log_action := 'Insert';
+  ELSIF UPDATING THEN
+    log_action := 'Update';
+  ELSIF DELETING THEN
+    log_action := 'Delete';
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('This code is not reachable.');
+  END IF;
+
+  INSERT INTO logger (log_id, log_operation, change_table, change_date)
+    VALUES (logger_id.nextval, log_action, 'Operations', SYSDATE);
+END;
+
+
+
+/* Usuwanie wszystkich tabel 
+DROP TABLE OPERATIONS;
+DROP TABLE EXPENSES;
+DROP TABLE PROFIT;
+DROP TABLE ORDERS;
+DROP TABLE PEOPLE;
+DROP TABLE PAYMENTS;
+DROP TABLE COMPANY;
+DROP TABLE WORKERS;
+DROP TABLE CLIENTS; 
+DROP SEQUENCE logger_id
+DROP TRIGGER COMPANY_CHANGES_TRIGGER
+DROP TRIGGER PAYMENTS_CHANGES_TRIGGER
+DROP TRIGGER EXPENSES_CHANGES_TRIGGER
+DROP TRIGGER PROFIT_CHANGES_TRIGGER
+DROP TRIGGER ORDERS_CHANGES_TRIGGER
+DROP TRIGGER CLIENTS_CHANGES_TRIGGER
+DROP TRIGGER WORKERS_CHANGES_TRIGGER
+DROP TRIGGER PEOPLE_CHANGES_TRIGGER
+DROP TRIGGER OPERATIONS_CHANGES_TRIGGER */
